98f3e9475b51c4b32275b5355fb2e40496fc137a
vnaranjo161 <vnaranjo161@gmail.com>
2026-03-05 10:43:19 -0500

    feature(register): auth service implementation (httpClient)

---
+++ src/app/features/auth/services/auth.service.ts
@@ -0,0 +1,15 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
+import { AuthResponse, RegisterRequest } from '../interfaces/auth.interfaces';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+
+  private http = inject(HttpClient);
+
+  register(data: RegisterRequest) {
+    return this.http.post<AuthResponse>(`${environment.apiUrl}/auth/register`, data);
+  }
+  
+}
